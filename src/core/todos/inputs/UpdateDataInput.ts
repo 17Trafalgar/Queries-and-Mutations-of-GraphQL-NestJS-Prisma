3ea29fe0e6ae3a824653ccd278d51d161e3f7be5
17Tralfagar <rybckin.rae2013@yandex.ru>
2023-01-16 12:46:00 +0000

    fix: UpdateDataInput and Makefile

--- src/core/todos/inputs/UpdateDataInput.ts
+++ src/core/todos/inputs/UpdateDataInput.ts
@@ -1,5 +1,26 @@
-import { OmitType, InputType, Field } from "@nestjs/graphql";
-import { Todo } from "../dto/Todo.dto";
+import { OmitType, InputType, Field, Int } from '@nestjs/graphql';
+import { TodoStatus } from '../dto/Status.dto';
+import { Todo } from '../dto/Todo.dto';
+
+//@InputType()
+//export class UpdateDataInput extends OmitType(Todo, ["id"] as const) {}
 
 @InputType()
-export class UpdateDataInput extends OmitType(Todo, ["id"] as const) {}
+export class UpdateDataInput implements Todo {
+  id: number;
+
+  @Field(() => String, { nullable: true })
+  name: string;
+
+  @Field(() => String, { nullable: true })
+  description: string;
+
+  @Field(() => Int, { nullable: true })
+  expires: number;
+
+  @Field(() => Boolean, { nullable: true })
+  isCompleted: boolean;
+
+  @Field(() => TodoStatus, { nullable: true })
+  status: TodoStatus;
+}
